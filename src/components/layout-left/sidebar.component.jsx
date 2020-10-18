cc2265cad9cd7530181a9fdc1c3cc5467b7d5bc1
kujebe <kujebeseun@gmail.com>
2020-10-18 18:08:15 +0100

    Added logo to sidebar

--- src/components/layout-left/sidebar.component.jsx
+++ src/components/layout-left/sidebar.component.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 
+import logo from "assets/images/logo.png";
+
 import styles from "./sidebar.module.scss";
 
 const Sidebar = () => {
     return (
-        <div className={styles.wrapper}>Sidebar</div>
+        <div className={styles.wrapper}>
+            <div className={styles.logo_wrapper}>
+                <img src={logo} alt="Logo" className={styles.logo} />
+            </div>
+        </div>
     )
 }
 
