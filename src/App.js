e04902f336db1d8921081b5baa296ab50ddaacee
kujebe <kujebeseun@gmail.com>
2020-10-18 01:30:33 +0100

    Created inital pages and added routes to app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Switch, Route } from "react-router-dom";
+
 import logo from './logo.svg';
 import './App.css';
 
+import SearchDetailsPage from "pages/job-search-page/job-search.page.jsx";
+import UserProfilePage from "pages/user-profile-page/user-profile.page.jsx";
+import JobDetailsPage from "pages/job-details-page/job-details.page.jsx";
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Switch>
+        <Route exact path="/">
+          <SearchDetailsPage />
+        </Route>
+        <Route path="/profile">
+          <UserProfilePage />
+        </Route>
+        <Route exact path="/job/:id">
+          <JobDetailsPage />
+        </Route>
+      </Switch>
+    </Fragment>
   );
 }
 
---
+++ src/pages/job-details-page/job-details.page.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const JobDetailsPage = () => {
+    return (
+        <div>Job Details Page</div>
+    )
+}
+
+export default JobDetailsPage;
