9d61f2047dfb76fd448066c03b5249f88beca556
kujebe <kujebeseun@gmail.com>
2020-10-18 01:45:49 +0100

    Cleaned out default react files and styles and created a global theme

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React, { Fragment } from 'react';
 import { Switch, Route } from "react-router-dom";
 
-import logo from './logo.svg';
-import './App.css';
-
 import SearchDetailsPage from "pages/job-search-page/job-search.page.jsx";
 import UserProfilePage from "pages/user-profile-page/user-profile.page.jsx";
 import JobDetailsPage from "pages/job-details-page/job-details.page.jsx";
