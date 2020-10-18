4c2f04b44143db2b5cda74b1fd67f4bfb3557fc8
kujebe <kujebeseun@gmail.com>
2020-10-18 14:05:11 +0100

    Global API Url in App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import SearchDetailsPage from "pages/job-search-page/job-search.page.jsx";
 import UserProfilePage from "pages/user-profile-page/user-profile.page.jsx";
 import JobDetailsPage from "pages/job-details-page/job-details.page.jsx";
 
+const API_URL = "https://search.torre.co";
+
 
 function App() {
   return (
@@ -13,14 +15,16 @@ function App() {
       <Switch>
         <Route exact path="/">
           <SearchDetailsPage
-            apiUrl="https://search.torre.co/opportunities/_search/"
+            apiUrl={`${API_URL}/opportunities/_search/`}
             size={20} />
         </Route>
         <Route path="/profile">
-          <UserProfilePage />
+          <UserProfilePage
+            apiUrl={`${API_URL}/people/_search/`} />
         </Route>
         <Route exact path="/job/:id">
-          <JobDetailsPage />
+          <JobDetailsPage
+            apiUrl={`${API_URL}/opportunities/`} />
         </Route>
       </Switch>
     </Layout>
