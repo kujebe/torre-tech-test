46a7b0a54b8d71642e320efadadf8df386781e0d
kujebe <kujebeseun@gmail.com>
2020-10-19 02:59:34 +0100

    Refactored job card layout

--- src/components/job-card/job-card.component.jsx
+++ src/components/job-card/job-card.component.jsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
+
+import corporatePhoto from "assets/images/corporate.png";
 
 import styles from "./job-card.module.scss";
 
@@ -9,12 +11,24 @@ const JobCard = ({ job }) => {
         <div className={styles.wrapper}>
             <div className={styles.top}>
                 <div className={styles.company}>
-                    <div className={styles.logo}><img src={job.organizations[0].picture} alt="Company logo" /></div>
+                    <div className={styles.logo}>
+
+                        {job.organizations[0].picture ? <img src={job.organizations[0].picture} alt="Company logo" /> : <img src={corporatePhoto} alt="Company logo" />}
+                    </div>
                     <div className={styles.company_name}>{job.organizations[0].name}</div>
                 </div>
             </div>
             <div className={styles.bottom}>
                 <Link to={`job/${job.id}`} className={styles.job_title}>{job.objective}</Link>
+                <div className={styles.pay}>
+                    {job.compensation ?
+                        <Fragment>
+                            From: {job.compensation.data.currency + job.compensation.data.minAmount}
+                        </Fragment>
+                        :
+                        "Salary not specified"
+                    }
+                </div>
             </div>
         </div>
     )
